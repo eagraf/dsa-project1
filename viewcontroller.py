faf6ce58e6940ffde3f2d700b97c78f0fd968fbe
eagraf <graf.ethan@gmail.com>
2019-10-13 18:57:09 -0400

    Updates to viewcontroller

--- viewcontroller.py
+++ viewcontroller.py
@@ -1,52 +1,59 @@
 import sys
 import time
+import json
 import implementation as imp
 
 def main():
 	siteID = sys.argv[1]
-	knownHosts = sys.argv[2]
-	inputfile = sys.argv[3]
 
-	inputs = f.readlines()
-	inputs = [x.strip() for x in inputs]
+	with open('knownhosts.json') as hosts_file:
+		hosts = json.load(hosts_file)['hosts']
 
-	reservationList = list()
+	if len(sys.argv) >= 3:
+		handle_test_file()
+	else:
+		handle_user_input()
 
-	wuuuu= imp.WuuBern()
 
-	for input in inputs:
-		if input.isdigit():
-			time.sleep(int(input))
-			continue
-		words = input.split()
-		elif words[0] == "reserve":
-			#reservation behavior
-			if True:
-				print("Reservation submitted for " + words[1] + ".")
-			else:
-				print("Cannot schedule reservation for " + words[1] + ".")
+def read_stable_storage():
+	''' If a stable storage file was written in this directory, read it to load dictionary '''
+	pass
 
-		elif words[0] == "cancel":
-			#cancel behavior
-			print("Reserevation for " + words[1] + " cancelled.")
+def handle_user_input():
+	''' Main loop for handling user input. '''
+	print("Handling user input")
 
-		elif words[0] == "view":
-			#sort reservationList
-			for res in reservationList:
-				print(res)
-
-		elif words[0] == "log":
-			#print wuuuu.log IN SORTED ORDER
-
-		elif words[0] == "send":
-
-		elif words[0] == "sendall":
-
-		elif words[0] == "clock":
-			for i in range(len(wuuuu.myMC)):
+	command = input().split(" ")
+	while command[0] != 'quit':
+		if command[0] == "reserve":
+			print("reserve command received")
+		elif command[0] == "cancel":
+			print("cancel command received")
+		elif command[0] == "view":
+			print("view command received")
+		elif command[0] == "log":
+			print("log command received")
+		elif command[0] == "send":
+			print("send command received")
+		elif command[0] == "sendall":
+			print("sendall command received")
+		elif command[0] == "clock":
+			print("clock command received")
+			'''for i in range(len(wuuuu.myMC)):
 				for j in range(len(wuuuu.myMC[i]) -1):
 					print(wuuuu.myMC[i][j], end = " ")
-				print(wuuuu.myMC[i][-1])
-				
+				print(wuuuu.myMC[i][-1])'''
+		else:
+			print("invalid command")
+		
+		# Wait for next command
+		command = input().split(" ")
 
+	print("exiting...")
 
+def handle_test_file():
+	''' Execute a sequence of commands with timings rather than taking user input. '''
+	print("Handling test file")
+
+if __name__== "__main__":
+	main()
